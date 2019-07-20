33abf052467e3c6f3d2eb5067cf34e360a6dc368
Onethity <vasstog.ya@yandex.ru>
2019-07-20 20:02:05 +0500

    View class is created

---
+++ app/View.php
@@ -0,0 +1,16 @@
+<?php
+
+#
+#   View Class
+#
+
+class View {
+
+    const TPL_DIR = "tpl/";
+
+    public function render($template, $params = [])
+    {
+        extract($params);
+        include_once(self::TPL_DIR.$template);
+    }
+}
