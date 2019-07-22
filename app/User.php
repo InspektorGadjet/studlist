c341e399191d87d1583f2132fc79ec1fdae279a4
Onethity <vasstog.ya@yandex.ru>
2019-07-22 19:25:48 +0500

    2 new methods

--- app/User.php
+++ app/User.php
@@ -18,4 +18,21 @@ class User {
     public $birth_year;
     public $place;
     public $auth_key;
+
+    public static function get_random_auth_key() 
+    {
+        return substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'),1,32);
+    }
+
+    public function fill_from_post($request) 
+    {
+        $this->name = trim(strval($request['name'])) ?? '';
+        $this->surname = trim(strval($request['surname'])) ?? '';
+        $this->email = trim(strval($request['email'])) ?? '';
+        $this->group_number = trim(strval($request['group_number'])) ?? '';
+        $this->birth_year = trim(intval($request['birth_year'])) ?? '0';
+        $this->gender = trim(strval($request['gender'])) ?? '';
+        $this->exam_score = trim(intval($request['exam_score'])) ?? '0';
+        $this->place = trim(strval($request['place'])) ?? '';
+    }
 }
