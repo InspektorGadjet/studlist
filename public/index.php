d4ab9973f8a1c737f82947e600feba0765ed4982
Onethity <vasstog.ya@yandex.ru>
2019-07-23 21:57:50 +0500

    search and sort now works (not correct?)

--- public/index.php
+++ public/index.php
@@ -5,8 +5,45 @@
 
 include_once "../bootstrap.php";
 
-$users = $user_gateway->get_users('id');
+if(isset($_GET['note'])) { //вывод уведомлений в шаблон
+    switch($_GET['note'])
+    {
+        case 'registred':
+            $notification = "Вы успешно зарегистрировались";
+            break;
+        case 'updated':
+            $notification = "Ваши данные успешно обновлены";
+            break;
+    }
+}
 
+#
+#   Разбираем параметры запроса
+#
+
+
+#       SEARCH
+if(isset($_GET['search'])) { //запрос на поиск
+    $search = $_GET['search'];
+    $title = 'Студенты по запросу "' . $search . '"';
+} else {
+    $search = NULL;
+    $title = "Список студентов";
+}
+
+
+#       SORT_BY
+if(isset($_GET['sort_by'])) {
+    $sort_by = $_GET['sort_by'];
+} else {
+    $sort_by = 'id';
+}
+
+
+$users = $user_gateway->get_users($sort_by, true, $search);
 $view->render('index.phtml', [
     'users' => $users,
+    'notification' => $notification ?? '',
+    'title' => $title,
+    'current_page' => 'index',
 ]);
