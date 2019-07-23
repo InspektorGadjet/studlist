d49450ffe94b5ec97b71657a6e673859fa951620
Onethity <vasstog.ya@yandex.ru>
2019-07-23 23:17:55 +0500

    get_sort_link is created

--- app/View.php
+++ app/View.php
@@ -8,9 +8,19 @@ class View {
 
     const TPL_DIR = "../tpl/";
 
-    public function render($template, $params = [])
+    public function render($template, $params = []) //отрисовка шаблона
     {
         extract($params);
         include_once(self::TPL_DIR.$template);
+    }
+
+    public function get_sort_link($sort_by, $reverse) //генерация ссылок сортировки
+    {
+        $link =  "/index.php?".http_build_query(array_merge($_GET, [
+            'sort_by' => $sort_by,
+            'reverse' => $reverse,
+        ]));
+
+        return htmlspecialchars($link);
     }
 }
