d481905b6f5435285ec7331465e3e1f2c53515ea
Onethity <vasstog.ya@yandex.ru>
2019-07-26 01:01:02 +0500

    now it works

--- app/Paginator.php
+++ app/Paginator.php
@@ -7,14 +7,39 @@
 class PaginatorException extends Exception {}
 
 class Paginator {
-    public $total_items;
-    public $items_per_page;
-    public $current_page;
+    public $enabled;
+
+    private $total_items;
+    private $items_per_page;
+    private $current_page;
 
     public function __construct($total_items, $items_per_page, $current_page)
     {
         $this->total_items = $total_items;
         $this->items_per_page = $items_per_page;
         $this->current_page = $current_page;
+
+        if($total_items <= $items_per_page) { //выключаем пагинацию, если все элементы умещаются на одной странице
+            $this->enabled = false; 
+        } else {
+            $this->enabled = true;
+        }
+    }
+
+    public function get_total_pages_array() 
+    {
+        $total_pages = ceil($this->total_items / $this->items_per_page);
+        return range(1, $total_pages);
+    }
+
+    public function get_limit()
+    {
+        return $this->items_per_page;
+    }
+
+
+    public function get_offset()
+    {
+        return $this->items_per_page * ($this->current_page - 1);
     }
 }
